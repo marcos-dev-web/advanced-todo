1f34cdf997547171b2370fc77fd5e1d4c615f920
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-04-03 10:34:11 -0300

    click in the panel to let he on top

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,6 @@
+const minWidthItems = 650;
+const minWidthPreview = 850;
+
 class MenuPreview {
   constructor(config) {
     
@@ -11,20 +14,43 @@ class MenuPreview {
 
     this.state = {
       items: {
-        open: true,
+        open: false,
       },
       preview: {
-        open: true,
-      }
+        open: false,
+      },
+	  zIndex: 201,
     }
+
+	window.onresize = (e) => {
+		const width = e.currentTarget.innerWidth;
+
+		if (width <= minWidthItems && this.state.items.open === true) {
+			this.itemsBox.classList.remove(this.classOpenItems);
+			this.state.items.open = false;
+		}
+		
+		if (width <= minWidthPreview && this.state.preview.open === true) {
+			this.previewBox.classList.remove(this.classOpenPreview);
+			this.state.preview.open = false;
+		}
+	}
   }
 
   toggleOpenViews = (element, classToOpen, stateElement) => {
-    element.classList.toggle(classToOpen, stateElement.open);
     stateElement.open = !stateElement.open;
+    element.classList.toggle(classToOpen, stateElement.open);
+	element.style.zIndex = ++this.state.zIndex;
+  }
+
+  setZIndex(element) {
+	  element.style.zIndex = ++this.state.zIndex;
   }
 
   config = () => {
+	this.itemsBox.addEventListener('click', this.setZIndex.bind(this, (this.itemsBox)));
+	this.previewBox.addEventListener('click', this.setZIndex.bind(this, (this.previewBox)));
+
     this.menuItems.addEventListener('click', () => this.toggleOpenViews(
       this.itemsBox,
       this.classOpenItems,
@@ -36,6 +62,7 @@ class MenuPreview {
       this.classOpenPreview,
       this.state.preview,
     ))
+
   }
 }
 
